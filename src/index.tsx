e6714a980458700fe153b8cf764b9894989b00f4
ivan_st_dev <ivan@mashvisor.com>
2022-02-17 01:38:24 +0200

    helmet removed title changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <meta charSet="utf-8" />
-    </Helmet>
     <div>
       <App />
     </div>
